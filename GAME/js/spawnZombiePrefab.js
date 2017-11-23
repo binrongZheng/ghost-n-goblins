e813f9f3c4d15200c6472f76d6889a899fdd3e7b
Pau Blanes <paublanes.enti@gmail.com>
2017-11-23 11:26:00 +0100

    Creat classe spawn zombie i afegit al index

---
+++ GAME/js/spawnZombiePrefab.js
@@ -0,0 +1,12 @@
+var platformer = platformer || {};
+
+platformer.spawnZombiePrefab=function(game,x,y){
+    
+}
+
+platformer.spawnZombiePrefab.prototype=Object.create(Phaser.Sprite.prototype);
+platformer.spawnZombiePrefab.prototype.constructor=platformer.spawnZombiePrefab;
+platformer.spawnZombiePrefab.prototype.update = function () {
+    
+}
+    
