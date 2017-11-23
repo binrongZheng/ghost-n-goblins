92dcc5875dafd20603d3e2c78ac457931776c43d
binrong <binrongzheng@gmail.com>
2017-11-23 16:26:51 +0100

    +ADD collisoin bulletsEnemy

--- GAME/js/enemyBulletPrefab.js
+++ GAME/js/enemyBulletPrefab.js
@@ -3,7 +3,7 @@ var platformer = platformer || {};
 //serà el propi enemic qui posi la velocitat adecuada a la bullet
 platformer.enemyBulletPrefab=function(game,x,y,_enemyBullet_type,velX,velY){
     this.enemyBullet_type=_enemyBullet_type;
-    
+
     switch(this.enemyBullet_type){
         case 0: Phaser.Sprite.call(this,game,x,y,'ull');
             break;
@@ -12,16 +12,16 @@ platformer.enemyBulletPrefab=function(game,x,y,_enemyBullet_type,velX,velY){
     }
     //potser s'haurà d'afegir en un group............
     this.animations.add('ullAnim', [0,1,2,3],10,true);
-    
+
     game.add.existing(this);
     this.anchor.setTo(.5);
     this.checkWorldBounds=true;
     this.outOfBoundsKill=true;
-    
+
     //físiques
     game.physics.arcade.enable(this);
     this.body.allowGravity = false;
-    
+
     //animació
     if(this.enemyBullet_type ==0){
         this.animations.play('ullAnim');
@@ -34,9 +34,9 @@ platformer.enemyBulletPrefab=function(game,x,y,_enemyBullet_type,velX,velY){
     this.body.velocity.y = velY;
     //col·lisió amb jugador
 
-    
+
     this.bulletHitsPlayer = function(){ //aclarar en quin lloc es gestionen les col·lisions
-        
+
         this.kill();
     };
 
@@ -45,3 +45,13 @@ platformer.enemyBulletPrefab=function(game,x,y,_enemyBullet_type,velX,velY){
 
 platformer.enemyBulletPrefab.prototype=Object.create(Phaser.Sprite.prototype);
 platformer.enemyBulletPrefab.prototype.constructor=platformer.enemyBulletPrefab;
+
+
+platformer.enemyBulletPrefab.prototype.update = function () {
+  this.game.physics.arcade.collide (this, platformer.tutorial.hero,function (bullet, hero){
+      bullet.kill();
+      hero.killPlayer();
+      //sumar punts al HUD
+
+  });
+}
