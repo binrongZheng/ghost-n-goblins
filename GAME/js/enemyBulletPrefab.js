d79296583f79fd8422372efbda64b94ba6732022
binrongZheng <binrongzheng@gmail.com>
2017-11-25 04:58:37 +0100

    commit

--- GAME/js/enemyBulletPrefab.js
+++ GAME/js/enemyBulletPrefab.js
@@ -50,6 +50,7 @@ platformer.enemyBulletPrefab.prototype.constructor=platformer.enemyBulletPrefab;
 platformer.enemyBulletPrefab.prototype.update = function () {
   this.game.physics.arcade.collide (this, platformer.tutorial.hero,function (bullet, hero){
       bullet.kill();
+      hero.body.checkCollision.up=true;
       hero.killPlayer();
   });
 }
