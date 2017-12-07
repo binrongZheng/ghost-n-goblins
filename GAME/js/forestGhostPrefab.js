8931e7f37b1ed100961b11f8cdfc649be89fd148
Adrià Biarnés Belso <adriabiarnesbelso@enti.cat>
2017-12-07 01:40:07 +0100

    CANVIS forestGhost

--- GAME/js/forestGhostPrefab.js
+++ GAME/js/forestGhostPrefab.js
@@ -2,22 +2,28 @@ var platformer = platformer || {};
 
 platformer.forestGhostPrefab=function(game,x,y,_level){
     this.level	= _level;
-    Phaser.Sprite.call(this,game,x,y,'forestGhost'); //posar el spritesheet que toqui
+    Phaser.Sprite.call(this,game,x,y,'forestGhost');
     game.add.existing(this);
 	this.anchor.setTo(.5);
     
 	this.randomX	= game.rnd.realInRange(-1.5,1.5);	//per fer que apareixi en una altura random
+    this.hp = 80;
 
 	//Físiques
     game.physics.arcade.enable(this);
     this.body.allowGravity 	= false;
-	//this.body.velocity.x 	= -gameOptions.foresGhostSpeed;
 	this.body.immovable 	= true;
     		
     //animacions
     this.animations.add('fly', [0,1],8,true);
-    this.animations.play('fly');
+    this.animations.add('turning', [3,4],8,false);
+    this.animations.add('spawn', [8,7,6,5],6,false);
+    this.animations.add('shooting', [2],8,false);
+    this.animations.play('spawn');
     
+	//quan ha acabat l'animació de l'spawn, comença a moure's
+	this.events.onAnimationComplete.add(this.startMoving,this); 
+	
     //Quan mor, sumem punts i afegim una explosió
     this.events.onKilled.add(this.forestGhostPoints, this);
 };
@@ -26,11 +32,19 @@ platformer.forestGhostPrefab.prototype=Object.create(Phaser.Sprite.prototype);
 platformer.forestGhostPrefab.prototype.constructor=platformer.forestGhostPrefab;
 
 platformer.forestGhostPrefab.prototype.update = function () {
-    //es mor quan surt de la pantalla?
+    //mentres està fent spawn
+	//es mor quan s'allunya suficient de la pantalla (poden entrar i sortir)
     
 };
 
+platformer.forestGhostPrefab.prototype.startMoving = function () {
+	if(this.animations.currentAnim.name == "spawn"){
+		this.body.velocity.x 	= -gameOptions.foresGhostSpeed;
+		this.animations.play('fly');
+	}
+};	
+
 platformer.forestGhostPrefab.prototype.forestGhostPoints = function () {
 	this.level.hud.updateScore(100);
-	this.level.explosions.add(new platformer.explosionPrefab(this.level.game,this.x,this.y,1, this.level));
+	this.level.explosions.add(new platformer.explosionPrefab(this.level.game,this.x,this.y,0, this.level));
 };
