3cfd47670ce7edc91060228b8b952c0ac8c2a096
adria <adriabiarnesbelso@enti.cat>
2017-12-08 12:46:55 +0100

    CANVIS forestGhost
    Hi ha un bug que fa que es quedi tota la estona disparant

--- GAME/js/forestGhostPrefab.js
+++ GAME/js/forestGhostPrefab.js
@@ -8,13 +8,14 @@ platformer.forestGhostPrefab=function(game,x,y,_level){
     this.level          = _level;
     this.y              = game.rnd.realInRange(360,100);	                        //per fer que apareixi en una altura random
     this.hp             = 80;
-	//this.goal 	    = this.x - game.rnd.realInRange(300,gameOptions.gameWidth);
     this.goal           = this.x + 200;
-    this.states         = {Spawning:1, chasing:2, changingDir1:3, changingDir2:4};
+    this.states         = {Spawning:1, chasing:2, changingDir1:3, changingDir2:4, shooting:5};
     this.currentState   = this.states.Spawning;
     this.dir            = 1;                                                        //-1 -> esquerra, 1-> dreta
     this.verticalDist   = 50;
-    this.verticalSpeed  = this.verticalDist/0.5;                                   //0.5-> temps que triga a donar la volta
+    this.verticalSpeed  = this.verticalDist/0.5;                                    //0.5-> temps que triga a donar la volta
+    this.willShoot      = game.rnd.integerInRange(1,10) > 4 ? true : false;        //Si dispararà o no quan passi per sobre el jugador
+    this.hasShot       = false;
     
 	//Físiques
     game.physics.arcade.enable(this);
@@ -22,11 +23,11 @@ platformer.forestGhostPrefab=function(game,x,y,_level){
 	this.body.immovable 	= true;
     		
     //Animacions
-    this.animations.add('fly', 		[0,1],		8,true);
-    this.animations.add('turning1',	[3,4],		8,false);
-    this.animations.add('turning2',	[4,3],		8,false);
-    this.animations.add('spawn',	[8,7,6,5],	6,false);
-    this.animations.add('shooting',	[2],		8,false);
+    this.animations.add('fly',      [0,1],                  8,true);
+    this.animations.add('turning1',	[3,4],                  8,false);
+    this.animations.add('turning2',	[4,3],		            8,false);
+    this.animations.add('spawn',	[8,7,6,5],	            6,false);
+    this.animations.add('shooting',	[0,1,0,1,0,1,2,2,2],	8,false);
     this.animations.play('spawn');
     
 	//Quan ha acabat l'animació de l'spawn, comença a moure's
@@ -44,14 +45,37 @@ platformer.forestGhostPrefab.prototype.update = function () {
        if((this.dir < 0 && this.x < this.goal) || (this.dir > 0 && this.x > this.goal)){
            this.currentState = this.states.changingDir1;
     	   this.animations.play('turning1');
-           //this.body.velocity.x = 0;
            this.body.acceleration.x = -this.dir*350;
            if(this.y < 360){
                this.body.velocity.y = this.verticalSpeed;
            }
            console.log("y: "+this.y);
+       } else{
+           if( this.willShoot && !this.hasShot &&
+              (this.dir < 0 && this.x < this.level.hero.x) || (this.dir > 0 && this.x > this.level.hero.x) )
+           {
+               this.body.velocity.x = 0;
+               this.animations.play('shooting');
+               this.currentState = this.states.shooting;
+           }
+           
+           
        }
     }
+    if(this.currentState == this.states.shooting){
+        if(!this.hasShot && this.animations.frame == 2){
+            var bulletDir = new Phaser.Point(platformer.tutorial.hero.x-this.x,platformer.tutorial.hero.y-6-this.y);
+            bulletDir.normalize().multiply(gameOptions.eyeSpeed,gameOptions.eyeSpeed);
+            var bala = new platformer.enemyBulletPrefab(this.game,this.x,this.y,0,0,0, this.level);
+            this.hasShot = true;
+        }
+        if(this.animations.currentAnim.isFinished){
+            this.currentState = this.states.chasing;
+            this.animations.play('fly');
+            this.body.velocity.x = gameOptions.foresGhostSpeed*this.dir;
+        }
+    }
+    
     if(this.currentState == this.states.changingDir1){
        if(this.animations.currentAnim.isFinished){
            this.animations.play('turning2');
@@ -68,6 +92,8 @@ platformer.forestGhostPrefab.prototype.update = function () {
         this.setNewGoal(this);
         this.body.velocity.x = gameOptions.foresGhostSpeed*this.dir;
         this.currentState = this.states.chasing;
+        this.hasShot = false;
+        this.willShoot = this.game.rnd.integerInRange(1,10) > 4 ? true : false;
        }
     }
     
