11c71ad98d7eb5ea02fdbedfae78ba0055963240
Adrià Biarnés (3B) <adriabiarnesbelso@enti.cat>
2017-12-14 16:08:18 +0100

    refractor

--- GAME/js/credits.js
+++ GAME/js/credits.js
@@ -22,20 +22,20 @@ platformer.credits={
         this.textos = this.add.group();
         
         this.madeBy = platformer.game.add.bitmapText(gameOptions.gameWidth/2,  55,  'gngFont', 'MADE BY', this.textSize);
-        this.madeBy.tint		= '0xc40f0f';
+        this.madeBy.tint = '0xc40f0f';
         
         this.pau    = platformer.game.add.bitmapText(gameOptions.gameWidth/2,  5*gameOptions.gameHeight/12,  'gngFont', 'PAU BLANES', this.textSize);
         this.adria  = platformer.game.add.bitmapText(gameOptions.gameWidth/2,  7*gameOptions.gameHeight/12,  'gngFont', 'ADRIA BIARNES', this.textSize);
         this.bing   = platformer.game.add.bitmapText(gameOptions.gameWidth/2,  9*gameOptions.gameHeight/12,  'gngFont', 'ZHENG BINRONG', this.textSize);
         
-        this.madeBy.anchor.setTo(.5);
-        this.pau.anchor.setTo(.5);
-        this.adria.anchor.setTo(.5);
-        this.bing.anchor.setTo(.5);
-        this.madeBy.x = gameOptions.gameWidth/2;
-        this.pau.x = gameOptions.gameWidth/2;
-        this.adria.x = gameOptions.gameWidth/2;
-        this.bing.x = gameOptions.gameWidth/2;
+        this.madeBy.anchor.setTo(   .5);
+        this.pau.anchor.setTo(      .5);
+        this.adria.anchor.setTo(    .5);
+        this.bing.anchor.setTo(     .5);
+        this.madeBy.x   = gameOptions.gameWidth/2;
+        this.pau.x      = gameOptions.gameWidth/2;
+        this.adria.x    = gameOptions.gameWidth/2;
+        this.bing.x     = gameOptions.gameWidth/2;
         
         //CONTROLS
         this.escKey = this.game.input.keyboard.addKey(Phaser.Keyboard.ESC);
