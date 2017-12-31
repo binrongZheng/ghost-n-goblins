1ed36724d692087804a38b16df96093466f69f6d
binrongZheng <binrongzheng@gmail.com>
2017-12-31 19:02:19 +0100

    +Ranking

--- GAME/js/ranking.js
+++ GAME/js/ranking.js
@@ -18,12 +18,47 @@ platformer.ranking={
       // 	this.bg = this.game.add.sprite(gameOptions.gameWidth/2,gameOptions.gameHeight/2, 'bg');
       //  this.bg.anchor.setTo(.5);
         this.textSize = 30;
-        this.rankingSize=22.5;
+        this.rankingSize=20;
         this.textos = this.add.group();
 
         this.madeBy = platformer.game.add.bitmapText(gameOptions.gameWidth/2,  55,  'gngFont', 'RANKING', this.textSize);
         this.madeBy.tint = '0xc40f0f';
+        this.name=[];
+        this.score=[];
 
+        if(gameOptions.highScores.length==null){
+          for(var i=0;i<10;i++){
+            if(i==0)
+            this.name[i]    = platformer.game.add.bitmapText(gameOptions.gameWidth/10,  (3+i)*gameOptions.gameHeight/14,  'gngFont', 'TOP', this.rankingSize);
+            else if(i==1)
+            this.name[i]    = platformer.game.add.bitmapText(gameOptions.gameWidth/10,  (3+i)*gameOptions.gameHeight/14,  'gngFont', '2nd', this.rankingSize);
+            else if(i==2)
+            this.name[i]    = platformer.game.add.bitmapText(gameOptions.gameWidth/10,  (3+i)*gameOptions.gameHeight/14,  'gngFont', '3rd', this.rankingSize);
+            else
+            this.name[i]    = platformer.game.add.bitmapText(gameOptions.gameWidth/10,  (3+i)*gameOptions.gameHeight/14,  'gngFont', (i+1)+'th', this.rankingSize);
+          }
+        }
+        else{
+          for(var i=0;i<gameOptions.highScores.length;i++){
+            if(i==0){
+            this.name[i]    = platformer.game.add.bitmapText(gameOptions.gameWidth/10,  (3+i)*gameOptions.gameHeight/14,  'gngFont', 'TOP '+gameOptions.highScores[i].name, this.rankingSize);
+            this.score[i]    = platformer.game.add.bitmapText(gameOptions.gameWidth/10*5.7,  (3+i)*gameOptions.gameHeight/14, 'gngFont', gameOptions.highScores[i].score, this.rankingSize);
+            }
+            else if(i==1){
+              this.name[i]    = platformer.game.add.bitmapText(gameOptions.gameWidth/10,  (3+i)*gameOptions.gameHeight/14,  'gngFont', '2nd '+gameOptions.highScores[i].name, this.rankingSize);
+              this.score[i]    = platformer.game.add.bitmapText(gameOptions.gameWidth/10*5.7,  (3+i)*gameOptions.gameHeight/14,  'gngFont',gameOptions.highScores[i].score, this.rankingSize);
+            }
+            else if(i==2){
+              this.name[i]    = platformer.game.add.bitmapText(gameOptions.gameWidth/10,  (3+i)*gameOptions.gameHeight/14,  'gngFont', '3rd '+gameOptions.highScores[i].name, this.rankingSize);
+              this.score[i]    = platformer.game.add.bitmapText(gameOptions.gameWidth/10*5.7,  (3+i)*gameOptions.gameHeight/14,  'gngFont',gameOptions.highScores[i].score, this.rankingSize);
+            }
+            else{
+              this.name[i]    = platformer.game.add.bitmapText(gameOptions.gameWidth/10,  (3+i)*gameOptions.gameHeight/14,  'gngFont', (i+1)+'th '+gameOptions.highScores[i].name, this.rankingSize);
+              this.score[i]    = platformer.game.add.bitmapText(gameOptions.gameWidth/10*5.7,  (3+i)*gameOptions.gameHeight/14, 'gngFont', gameOptions.highScores[i].score, this.rankingSize);
+            }
+          }
+        }
+        /*
         this.first    = platformer.game.add.bitmapText(gameOptions.gameWidth/10,  3*gameOptions.gameHeight/14,  'gngFont', 'TOP', this.rankingSize);
         this.second  = platformer.game.add.bitmapText(gameOptions.gameWidth/10,  4*gameOptions.gameHeight/14,  'gngFont', '2nd', this.rankingSize);
         this.third   = platformer.game.add.bitmapText(gameOptions.gameWidth/10,  5*gameOptions.gameHeight/14,  'gngFont', '3rd', this.rankingSize);
@@ -34,7 +69,7 @@ platformer.ranking={
         this.eight    = platformer.game.add.bitmapText(gameOptions.gameWidth/10,  10*gameOptions.gameHeight/14,  'gngFont', '8th', this.rankingSize);
         this.nine  = platformer.game.add.bitmapText(gameOptions.gameWidth/10,  11*gameOptions.gameHeight/14,  'gngFont', '9th', this.rankingSize);
         this.ten   = platformer.game.add.bitmapText(gameOptions.gameWidth/10,  12*gameOptions.gameHeight/14,  'gngFont', '10th', this.rankingSize);
-
+*/
         this.madeBy.anchor.setTo( .5);
         //this.first.anchor.setTo(  .5);
         //this.second.anchor.setTo( .5);
@@ -50,7 +85,7 @@ platformer.ranking={
 
         //CONTROLS
         this.escKey = this.game.input.keyboard.addKey(Phaser.Keyboard.ESC);
-        
+
     },
     update:function(){
         if(this.escKey.isDown){
